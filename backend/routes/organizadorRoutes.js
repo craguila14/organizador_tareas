a406e837570d7d668aa47c42a3cd0d5ac6c16e17
craguila14 <craguilaasenjo@gmail.com>
2025-04-24 21:41:32 -0400

    detalles

--- backend/routes/organizadorRoutes.js
+++ backend/routes/organizadorRoutes.js
@@ -8,27 +8,27 @@ import { middleware } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 //usuario
-router.post('/registrarse', userController.registerUser)  //check
-router.post('/login', userController.loginUser) //check
-router.get('/usuario', middleware.authenticateToken, userController.getUser); //check
+router.post('/registrarse', userController.registerUser)  
+router.post('/login', userController.loginUser) 
+router.get('/usuario', middleware.authenticateToken, userController.getUser); 
 
 //boards
-router.get('/boards', middleware.authenticateToken, boardsController.getBoardsByUser); //check
-router.get('/boards/:id', middleware.authenticateToken, boardsController.getBoardById); //check
-router.post('/boards', middleware.authenticateToken, boardsController.createBoard); //check
-router.put('/boards/:id', middleware.authenticateToken, boardsController.updateBoard); //check
-router.delete('/boards/:id', middleware.authenticateToken, boardsController.deleteBoard); //check
+router.get('/boards', middleware.authenticateToken, boardsController.getBoardsByUser); 
+router.get('/boards/:id', middleware.authenticateToken, boardsController.getBoardById);
+router.post('/boards', middleware.authenticateToken, boardsController.createBoard); 
+router.put('/boards/:id', middleware.authenticateToken, boardsController.updateBoard); 
+router.delete('/boards/:id', middleware.authenticateToken, boardsController.deleteBoard);
 
 //lists
-router.get('/boards/:boardId/lists', middleware.authenticateToken, listsController.getListsByBoard); //check
-router.post('/boards/:boardId/lists', middleware.authenticateToken, listsController.createList); //check
-router.put('/lists/:id', middleware.authenticateToken, listsController.updateList); //check
-router.delete('/lists/:id', middleware.authenticateToken, listsController.deleteList); //che
+router.get('/boards/:boardId/lists', middleware.authenticateToken, listsController.getListsByBoard); 
+router.post('/boards/:boardId/lists', middleware.authenticateToken, listsController.createList); 
+router.put('/lists/:id', middleware.authenticateToken, listsController.updateList); 
+router.delete('/lists/:id', middleware.authenticateToken, listsController.deleteList); 
 
 //cards
-router.get('/lists/:listId/cards', middleware.authenticateToken, cardsController.getCardsByList);
+router.get('/lists/:listId/cards', middleware.authenticateToken, cardsController.getCardsByList); 
 router.post('/lists/:listId/cards', middleware.authenticateToken, cardsController.createCard); 
-router.put('/cards/:id', middleware.authenticateToken, cardsController.updateCard)
+router.put('/cards/:id', middleware.authenticateToken, cardsController.updateCard) 
 router.delete('/cards/:id', middleware.authenticateToken, cardsController.deleteCard);
 
 
