3b5fdef283061e1a3fea8d6eef9b3f31c22dfbc3
craguila14 <craguilaasenjo@gmail.com>
2025-07-31 16:39:49 -0400

    index update

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes/organizadorRoutes.js';
+import pkg from 'pg'
 
 dotenv.config();
+
+const { Client } = pkg;
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect()
+  .then(() => console.log('✅ Conectado a la base de datos'))
+  .catch(err => console.error('❌ Error de conexión a la base de datos:', err));
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://organizador-tareas-0smh.onrender.com'
+    // apiUrl: 'https://organizador-tareas-0smh.onrender.com'
+    apiUrl: 'http://localhost:3000',
+
   };
   
