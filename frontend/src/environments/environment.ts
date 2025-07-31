28915789f71d4b4a5d5039c147ed38ef7ab61e45
craguila14 <craguilaasenjo@gmail.com>
2025-07-31 18:57:44 -0400

    env update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://organizador-tareas-0smh.onrender.com'
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://organizador-tareas-0smh.onrender.com'
+    // apiUrl: 'http://localhost:3000',
 
   };
   
