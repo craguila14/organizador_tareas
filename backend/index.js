6e539c9e3c558d22aea17ec873f7a44c949fed8b
craguila14 <craguilaasenjo@gmail.com>
2025-07-31 16:54:54 -0400

    index update

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const client = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  ssl: true
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 
